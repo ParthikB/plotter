2369685a20f3fcc2d82b20d1951b0919c24490d6
Parthik Bhandari <connect2parthik@gmail.com>
2019-12-31 20:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import dash
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
 import dash_html_components as html
 import pandas as pd
-from pandas_datareader.data import DataReader
-import time
-from collections import deque
 import plotly.graph_objs as go
-import random
 
 
-app = dash.Dash('vehicle-data')
+app = dash.Dash('Quick Plotter')
 
 data = pd.read_csv('datasets/iris.csv')
 
@@ -20,48 +17,66 @@ for col in data.columns:
 
 app.layout = html.Div([
     html.Div([
-        html.H2('Plotter Analysis',
+        html.H2('Quick Plotter',
                 style={'float': 'left',
                        }),
-        ]),
-    dcc.Dropdown(id='data-name',
+        
+    dcc.Dropdown(id='x_axis',
                  options=[{'label': s, 'value': s} for s in data_dict.keys()],
-                 value=['Id', 'SepalLengthCm'],
-                 multi=True
+                 placeholder='Select x-axis',
+                 multi=False
                  ),
-    html.Div(children=html.Div(id='graphs'), className='row'),
-    ], className="container",style={'width':'98%','margin-left':10,'margin-right':10,'max-width':50000})
+    dcc.Dropdown(id='y_axis',
+                 options=[{'label': s, 'value': s} for s in data_dict.keys()],
+                 placeholder='Select y-axis',
+                 multi=False
+                 ),
+    # dcc.RadioItems(id='plot_type',
+    #             options=[{'label': 'Line',          'value': 'lines'},
+    #                     {'label': 'Scatter',       'value': 'markers'},
+    #                     {'label': "Bring 'em all", 'value': 'lines+markers'}],
+    #             value='lines')
+    dcc.Dropdown(id='plot_type',
+                 options=[{'label': 'Line',          'value': 'lines'},
+                         {'label': 'Scatter',       'value': 'markers'},
+                         {'label': "Bring 'em all", 'value': 'lines+markers'}],    #              placeholder='Select Plot type',
+                 multi=False)
+                 ],
+    style={'width':'40%'}),
+    html.Div(children=html.Div(id='graphs'), className='row')], 
+    className="container",
+    style={'width':'98%',
+            'margin-left':10,
+            'margin-right':10,
+            'max-width':50000})
 
 
 @app.callback(
-    dash.dependencies.Output('graphs','children'),
-    [dash.dependencies.Input('data-name', 'value')],
+    Output('graphs','children'),
+    [Input('x_axis', 'value'), 
+    Input('y_axis', 'value'),
+    Input('plot_type', 'value')],
     )
-def update_graph(data_names):
+def update_graph(x_axis, y_axis, plot_type):
     graphs = []
     class_choice = 'col s12'
+    x, y = x_axis, y_axis
 
-    # for data_name in data_names:
-    # print(data_names)
-    if data_names and len(data_names) == 2:
-        # print(len(data_names))
-        x, y = data_names
+    if x and y:
         data = go.Scatter(
-            x=list(data_dict[x]),
-            y=list(data_dict[y]),
-            name='Line Plot',
-            mode='markers'
+            x       = list(data_dict[x]),
+            y       = list(data_dict[y]),
+            name    = 'Line Plot',
+            mode    = plot_type
             )
-        # print(list(data_dict[x]))
 
         graphs.append(html.Div(dcc.Graph(
             id='data_names',
             animate=False,
-            # figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(data_dict[x]),max(data_dict[x])]),
-            #                                             yaxis=dict(range=[min(data_dict[y]),max(data_dict[y])]),
-            #                                             margin={'l':50,'r':1,'t':45,'b':1},
-            #                                             title=f'{x} v/s {y}')}
-            figure={'data': [data],'layout' : go.Layout(title=f'{x} v/s {y}')}), 
+            figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(data_dict[x]),max(data_dict[x])]),
+                                                        yaxis=dict(range=[min(data_dict[y]),max(data_dict[y])]),
+                                                        # margin={'l':50,'r':1,'t':45,'b':1},
+                                                        title=f'{x} v/s {y}')}),
             className=class_choice))
 
         return graphs
