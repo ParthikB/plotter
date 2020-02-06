d2d4f42831ed3804299b22c80340558ce779b24b
Parthik Bhandari <connect2parthik@gmail.com>
2020-02-06 23:33:09 +0530

    Add files via upload

--- gui/main.py
+++ gui/main.py
@@ -14,84 +14,14 @@ from tkinter import ttk
 import pandas as pd
 
 
-
+# Initializing few Constants
 LARGE_FONT = ('Verdana', 12)
 
 f = Figure(figsize=(5, 5), dpi=100)
 ax1 = f.add_subplot(111)
 
-class Command:
 
-	def update_graph(self, plot_type, plot_val_x, plot_val_y):
-		ax1.clear()
-		if plot_type == 'line':
-			print('plotting line')
-			ax1.plot(dataframe[plot_val_x], dataframe[plot_val_y])
-		elif plot_type == 'scatter':
-			print('plotting scatter')
-			ax1.scatter(dataframe[plot_val_x], dataframe[plot_val_y])
-		ax1.grid(1)
-		ax1.set_xlabel(plot_val_x)
-		ax1.set_ylabel(plot_val_y)
-		
-		canvas.draw()
-
-
-	def browse_file(self, root):
-		root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
-		global dataframe, feature_list, plot_val_x, plot_val_y
-		
-		dataframe = pd.read_csv(root.filename)
-		
-		feature_list = []
-		for col in dataframe.columns:
-			feature_list.append(col)
-		
-		plot_val_x  = tk.StringVar()
-		plot_val_x.set(feature_list[0])
-		popupMenu_x = tk.OptionMenu(root, plot_val_x, *feature_list)
-		popupMenu_x.pack()
-		# plot_val_x  = plot_val_x.get()
-		
-		plot_val_y  = tk.StringVar()
-		plot_val_y.set(feature_list[0])
-		popupMenu_y = tk.OptionMenu(root, plot_val_y, *feature_list)
-		popupMenu_y.pack()
-		# plot_val_y  = plot_val_y.get()
-
-		print(plot_val_x, plot_val_y)
-		# print(plot_val_x.get(), plot_val_y.get())
-		print('dataframe loaded.')
-		# print(plot_val_x.get(), plot_val_y.get())
-
-
-	def plot(self, root, filename, plot_type, plot_val_x, plot_val_y):
-		print('*****************************************')
-		print("********************", plot_val_x, plot_val_y)
-		ax1.clear()
-		if plot_type == 'line':
-			print('plotting line')
-			ax1.plot(dataframe[plot_val_x], dataframe[plot_val_y])
-		elif plot_type == 'scatter':
-			print('plotting scatter')
-			ax1.scatter(dataframe[plot_val_x], dataframe[plot_val_y])
-		ax1.grid(1)
-		ax1.set_xlabel(plot_val_x)
-		ax1.set_ylabel(plot_val_y)
-
-		global canvas
-		canvas = FigureCanvasTkAgg(f, root)
-		canvas.get_tk_widget().pack(side=tk.RIGHT, fill='both', expand=True)
-		print('Canvas created.')
-		canvas.draw()
-
-		toolbar = NavigationToolbar2Tk(canvas, root)
-		toolbar.update()
-		canvas._tkcanvas.pack(side=tk.TOP, fill='both', expand=True)
-		
-
-
-
+# Main GUI Base-Class
 class App(tk.Tk):
 
 	def __init__(self, *arg, **kwargs):
@@ -120,9 +50,73 @@ class App(tk.Tk):
 		frame.tkraise()
 
 
+# Helper Class that carries various commands.
+class Command:
+
+	# Update the graph with every new selection
+	def update_graph(self, plot_type, plot_val_x, plot_val_y):
+		ax1.clear() # clears out the previous data on the graph so that it doesn't get rendered again
+		
+		# Plotting the graph according the the type of plot
+		if plot_type == 'line':
+			# print('Plotting line')
+			ax1.plot(dataframe[plot_val_x], dataframe[plot_val_y])
+		elif plot_type == 'scatter':
+			# print('Plotting scatter')
+			ax1.scatter(dataframe[plot_val_x], dataframe[plot_val_y])
+		
+		ax1.grid(1)
+		ax1.set_xlabel(plot_val_x)
+		ax1.set_ylabel(plot_val_y)
+		
+		# Rendering the graph onto the GUI
+		canvas.draw()
+
+
+	# Browses the file on the local machine
+	def browse_file(self, root):
+
+		root.filename =  filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("csv files","*.csv"), ("all files", "*.*")))
+		
+		# Initializing some global variables that are needed afterward in the program
+		global dataframe, feature_list, plot_val_x, plot_val_y
+		
+		dataframe = pd.read_csv(root.filename) # Reading the .csv dataset file
+		
+		# Creating a feature list of the columns of the dataset to be fed to the dropdown menu
+		feature_list = []
+		for col in dataframe.columns:
+			feature_list.append(col)
+		
+		# Selecting the X-AXIS feature
+		plot_val_x  = tk.StringVar()
+		plot_val_x.set(feature_list[0]) # Setting the default value for the variable
+		popupMenu_x = tk.OptionMenu(root, plot_val_x, *feature_list)
+		popupMenu_x.pack()
+		
+		# Selecting the Y-AXIS feature
+		plot_val_y  = tk.StringVar()
+		plot_val_y.set(feature_list[0])
+		popupMenu_y = tk.OptionMenu(root, plot_val_y, *feature_list)
+		popupMenu_y.pack()
+
+		# print(plot_val_x, plot_val_y)
+		print('Dataframe loaded.')
+		
+		# Creating a Blank Canvas for the matplotlib graphs
+		global canvas
+		canvas = FigureCanvasTkAgg(f, root)
+		canvas.get_tk_widget().pack(side=tk.RIGHT, fill='both', expand=True)
+		print('Canvas created.')
+		canvas.draw()
+
+		toolbar = NavigationToolbar2Tk(canvas, root)
+		toolbar.update()
+		canvas._tkcanvas.pack(side=tk.TOP, fill='both', expand=True)
 
 
 
+# The HOMEPAGE of the GUI
 class StartPage(tk.Frame):
 
 	def __init__(self, parent, controller):
@@ -130,43 +124,35 @@ class StartPage(tk.Frame):
 		
 
 		label = tk.Label(self, 
-						text='Now this is where it starts!', 
+						text='Now this is where it begins!', 
 						font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
+		# the UPLOAD button
 		button1 = ttk.Button(self, text='Upload',
 							command=lambda: command.browse_file(self))
 		button1.pack()
 
-		# feature_list = ['none']		
-		# plot_vals = tk.StringVar()
-		# popupMenu = tk.OptionMenu(self, plot_vals, *feature_list)
-		# popupMenu.pack()
 
-		global plot_type
-		plot_type = tk.StringVar()
+		# Options to select the PLOT TYPE
 		radio1 = ttk.Radiobutton(self, 
 								text='Line', 
-								variable=plot_type, 
 								value='line',
 								command=lambda: command.update_graph('line', plot_val_x.get(), plot_val_y.get()))
 		radio1.pack(anchor='w')
 
 		radio2 = ttk.Radiobutton(self, 
 								text='Scatter', 
-								variable=plot_type, 
 								value='scatter',
 								command=lambda: command.update_graph('scatter', plot_val_x.get(), plot_val_y.get()))
 		radio2.pack(anchor='w')
 
 
-		button2 = ttk.Button(self, text='Plot',
-							command=lambda: command.plot(self, dataframe, plot_type.get(), plot_val_x.get(), plot_val_y.get()))
-		button2.pack()
 
 
+if __name__ == '__main__':
+	
+	command = Command()
 
-command = Command()
-
-app = App()
-app.mainloop()
+	app = App()
+	app.mainloop()
