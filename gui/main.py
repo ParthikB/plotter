932a91837a78df4c5532dd3125d40659ed390c02
Parthik Bhandari <connect2parthik@gmail.com>
2020-02-06 19:57:03 +0530

    Add files via upload

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib import style
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
 
 LARGE_FONT = ('Verdana', 12)
 
@@ -6,12 +15,13 @@ LARGE_FONT = ('Verdana', 12)
 class App(tk.Tk):
 
 	def __init__(self, *arg, **kwargs):
-
 		tk.Tk.__init__(self, *arg, **kwargs)
+
+		# tk.Tk.iconbitmap(self, default='path_to_icon.ico')
+		tk.Tk.wm_title(self, 'Quick Plotter')
+
 		container = tk.Frame(self)
-
 		container.pack(side='top', fill='both', expand=True)
-
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
 
@@ -40,10 +50,11 @@ class StartPage(tk.Frame):
 		label = tk.Label(self, text='Now this is where it starts!', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		button1 = tk.Button(self, text='Upload',
+		button1 = ttk.Button(self, text='Upload',
 			command=lambda: controller.show_frame(PageOne))
 		button1.pack()
 
+		
 
 class PageOne(tk.Frame):
 
@@ -53,10 +64,21 @@ class PageOne(tk.Frame):
 		label = tk.Label(self, text='Page two', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		button1 = tk.Button(self, text='Home',
+		button1 = ttk.Button(self, text='Home',
 			command=lambda: controller.show_frame(StartPage))
 		button1.pack()
+		
+		f = Figure(figsize=(5, 5), dpi=100)
+		ax1 = f.add_subplot(111)
+		ax1.plot(range(10))
 
+		canvas = FigureCanvasTkAgg(f, self)
+		canvas.draw()
+		canvas.get_tk_widget().pack(side=tk.TOP, fill='both', expand=True)
+
+		toolbar = NavigationToolbar2Tk(canvas, self)
+		toolbar.update()
+		canvas._tkcanvas.pack(side=tk.TOP, fill='both', expand=True)
 
 app = App()
 app.mainloop()
