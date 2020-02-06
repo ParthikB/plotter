4b433ee6919679d42a80a327d36cc6a727667742
Parthik Bhandari <connect2parthik@gmail.com>
2020-02-06 19:09:01 +0530

    tkinter gui

---
+++ gui/main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+LARGE_FONT = ('Verdana', 12)
+
+
+class App(tk.Tk):
+
+	def __init__(self, *arg, **kwargs):
+
+		tk.Tk.__init__(self, *arg, **kwargs)
+		container = tk.Frame(self)
+
+		container.pack(side='top', fill='both', expand=True)
+
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
+
+		self.frames={}
+
+		frame = StartPage(container, self)
+
+		self.frames[StartPage] = frame
+
+		frame.grid(row=0, column=0, sticky='ew')
+
+		self.show_frame(StartPage)
+
+
+	def show_frame(self, cont):
+		frame = self.frames[cont]
+		frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text='Now this is where it starts!', font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+
+
+
+app = App()
+app.mainloop()
