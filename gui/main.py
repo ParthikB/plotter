e3e8b24cd7a73648b9c9477e036cdbf6bd517f26
Parthik Bhandari <connect2parthik@gmail.com>
2020-02-07 10:18:36 +0530

    Add files via upload

--- gui/main.py
+++ gui/main.py
@@ -17,7 +17,7 @@ import pandas as pd
 # Initializing few Constants
 LARGE_FONT = ('Verdana', 12)
 
-f = Figure(figsize=(5, 5), dpi=100)
+f = Figure(figsize=(15, 5), dpi=100)
 ax1 = f.add_subplot(111)
 
 
@@ -31,7 +31,7 @@ class App(tk.Tk):
 		tk.Tk.wm_title(self, 'Quick Plotter')
 
 		container = tk.Frame(self)
-		container.pack(side='top', fill='both', expand=True)
+		container.grid()
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
 
@@ -41,7 +41,7 @@ class App(tk.Tk):
 			frame = F(container, self)
 			self.frames[F] = frame
 			# frame.grid(row=0, column=0, sticky='ew')
-			frame.pack()
+			frame.grid()
 
 		self.show_frame(StartPage)
 
@@ -99,19 +99,21 @@ class Command:
 		
 		FEATURES = tk.Frame(INFO)
 
-		tk.Label(FEATURES, text='Features :').grid(row=0)
+		tk.Label(FEATURES, text='Features :').grid(row=0, columnspan=2)
 
 		# Selecting the X-AXIS feature
 		plot_val_x  = tk.StringVar()
 		plot_val_x.set(feature_list[0]) # Setting the default value for the variable
-		popupMenu_x = tk.OptionMenu(FEATURES, plot_val_x, command=lambda x: command.update_graph('line', plot_val_x.get(), plot_val_y.get()), *feature_list).grid(row=1)
+		tk.Label(FEATURES, text='X-Axis :').grid(row=1, column=0)
+		popupMenu_x = tk.OptionMenu(FEATURES, plot_val_x, command=lambda x: command.update_graph('line', plot_val_x.get(), plot_val_y.get()), *feature_list).grid(row=1, column=1)
 		
 		# Selecting the Y-AXIS feature
 		plot_val_y  = tk.StringVar()
 		plot_val_y.set(feature_list[0])
-		popupMenu_y = tk.OptionMenu(FEATURES, plot_val_y, command=lambda x: command.update_graph('line', plot_val_x.get(), plot_val_y.get()), *feature_list).grid(row=2)
+		tk.Label(FEATURES, text='Y-Axis :').grid(row=2, column=0)
+		popupMenu_y = tk.OptionMenu(FEATURES, plot_val_y, command=lambda x: command.update_graph('line', plot_val_x.get(), plot_val_y.get()), *feature_list).grid(row=2, column=1)
 		
-		FEATURES.pack(pady=30)
+		FEATURES.grid()
 
 
 		OPTIONS = tk.Frame(INFO)
@@ -129,11 +131,11 @@ class Command:
 						value='scatter',
 						command=lambda: command.update_graph('scatter', plot_val_x.get(), plot_val_y.get())).grid(row=2, sticky='w')
 
-		OPTIONS.pack(pady=30)
+		OPTIONS.grid()
 
 
 
-		INFO.pack(side='left', padx=15)
+		INFO.grid(row=1, column=0)
 #####################################################################################################################################
 
 
@@ -143,16 +145,16 @@ class Command:
 		# Creating a Blank Canvas for the matplotlib graphs
 		global canvas
 		canvas = FigureCanvasTkAgg(f, root)
-		canvas.get_tk_widget().pack(side=tk.RIGHT, fill='both', expand=True)
+		canvas.get_tk_widget().grid(row=0)
 		print('Canvas created.')
 		canvas.draw()
 
-		toolbar = NavigationToolbar2Tk(canvas, root)
-		toolbar.update()
-		canvas._tkcanvas.pack(side=tk.TOP, fill='both', expand=True)
+		# toolbar = NavigationToolbar2Tk(canvas, root)
+		# toolbar.update()
+		# canvas._tkcanvas.grid(row=0)
 
 
-		GRAPH.pack(side='right', fill='both', expand=True)
+		GRAPH.grid(rowspan=2, column=1)
 #####################################################################################################################################
 
 
@@ -166,14 +168,17 @@ class StartPage(tk.Frame):
 		# INFO = tk.Frame(parent)
 		INFO = parent
 
-		label = tk.Label(INFO, 
+		WELCOME = tk.Frame(parent)
+
+		label = tk.Label(WELCOME, 
 						text='It begins here!', 
-						font=LARGE_FONT).pack()
+						font=LARGE_FONT).grid(row=0)
 
 		# the UPLOAD button
-		button1 = ttk.Button(INFO, text='Upload',
-							command=lambda: command.browse_file(INFO, parent)).pack()
+		button1 = ttk.Button(WELCOME, text='Upload',
+							command=lambda: command.browse_file(INFO, parent)).grid(row=1)
 
+		WELCOME.grid(row=0, column=0)
 		# INFO.pack(side='left')
 
 
