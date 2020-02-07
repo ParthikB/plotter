95fdef6afc0253e060a4adcc3a64214673ccdee5
Parthik Bhandari <connect2parthik@gmail.com>
2020-02-08 04:46:04 +0530

    Update main.py

--- gui/main.py
+++ gui/main.py
@@ -2,8 +2,11 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 from matplotlib import style
+import matplotlib.pyplot as plt
 # style.use('ggplot')
-style.use('seaborn')
+# style.use('seaborn')
+style.use('dark_background')
+
 
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -16,12 +19,20 @@ import pandas as pd
 
 
 # Initializing few Constants
-LARGE_FONT = ('Verdana', 12)
-BACKGROUND_COLOR = '#42f498'
+LARGE_FONT = ('Laksaman', 12)
+# BACKGROUND_COLOR = '#42f498'
+# FONT_COLOR = '#121212'
+BACKGROUND_COLOR = '#121212'
+FONT_COLOR = '#FFFFFF'
 
 
-f = Figure(figsize=(15, 5), dpi=100)
+f = Figure(figsize= (15, 5), 
+           dpi    = 100)
 ax1 = f.add_subplot(111)
+f.subplots_adjust(left   = 0.07, 
+           bottom = 0.07, 
+           right  = 0.95, 
+           top    = 0.93)
 
 
 
@@ -65,19 +76,26 @@ class App(tk.Tk):
 # Helper Class that carries various commands.
 class Command:
 
+    def dark_mode(self):
+        global BACKGROUND_COLOR, FONT_COLOR
+        BACKGROUND_COLOR = '#121212'
+        FONT_COLOR = '#FFFFFF'
+        style.use('dark_background')
+
+
     # Update the graph with every new selection
-    def update_graph(self, plot_type, plot_val_x, plot_val_y):
+    def update_graph(self, plot_type, plot_val_x, plot_val_y, scale, grid_check):
         ax1.clear() # clears out the previous data on the graph so that it doesn't get rendered again
-        
+
         # Plotting the graph according the the type of plot
         if plot_type == 'line':
             # print('Plotting line')
-            ax1.plot(dataframe[plot_val_x], dataframe[plot_val_y])
+            ax1.plot(dataframe[plot_val_x], dataframe[plot_val_y], linewidth=scale)
         elif plot_type == 'scatter':
             # print('Plotting scatter')
-            ax1.scatter(dataframe[plot_val_x], dataframe[plot_val_y])
+            ax1.scatter(dataframe[plot_val_x], dataframe[plot_val_y], linewidth=scale)
         
-        ax1.grid(1)
+        ax1.grid(grid_check)
         ax1.set_xlabel(plot_val_x)
         ax1.set_ylabel(plot_val_y)
         
@@ -112,49 +130,129 @@ class Command:
         
         FEATURES = tk.Frame(INFO, bg=BACKGROUND_COLOR)
 
-        tk.Label(FEATURES, text='Features :', bg=BACKGROUND_COLOR).grid(row=0, columnspan=2)
+        tk.Label(FEATURES, 
+                 text='Features :', 
+                 bg=BACKGROUND_COLOR,
+                 fg=FONT_COLOR).grid(row=0, columnspan=2)
 
         # Selecting the X-AXIS feature
         plot_val_x  = tk.StringVar()
         plot_val_x.set(feature_list[0]) # Setting the default value for the variable
-        tk.Label(FEATURES, text='X-Axis :', bg=BACKGROUND_COLOR).grid(row=1, column=0)
-        popupMenu_x = tk.OptionMenu(FEATURES, plot_val_x, command=lambda x: command.update_graph('line', plot_val_x.get(), plot_val_y.get()), *feature_list).grid(row=1, column=1)
+        
+        tk.Label(FEATURES,
+                text='X-Axis :', 
+                bg=BACKGROUND_COLOR,
+                fg=FONT_COLOR).grid(row=1, column=0)
+
+        popupMenu_x = ttk.OptionMenu(FEATURES, 
+                                    plot_val_x, 
+                                    command=lambda x: command.update_graph(CURRENT_PLOT_TYPE.get(), 
+                                    plot_val_x.get(), 
+                                    plot_val_y.get(),
+                                    scale=SCALE.get(),
+                                    grid_check=GRID_CHECK.get()),
+                                    *feature_list).grid(row=1, column=1, sticky='w')
         
         # Selecting the Y-AXIS feature
         plot_val_y  = tk.StringVar()
         plot_val_y.set(feature_list[0])
-        tk.Label(FEATURES, text='Y-Axis :', bg=BACKGROUND_COLOR).grid(row=2, column=0)
-        popupMenu_y = tk.OptionMenu(FEATURES, plot_val_y, command=lambda x: command.update_graph('line', plot_val_x.get(), plot_val_y.get()), *feature_list).grid(row=2, column=1)
+        tk.Label(FEATURES, 
+                text='Y-Axis :', 
+                bg=BACKGROUND_COLOR,
+                fg=FONT_COLOR).grid(row=2, column=0)
+        
+        popupMenu_y = ttk.OptionMenu(FEATURES, 
+                                    plot_val_y, 
+                                    command=lambda x: command.update_graph(CURRENT_PLOT_TYPE.get(), 
+                                    plot_val_x.get(), 
+                                    plot_val_y.get(),
+                                    scale=SCALE.get(),
+                                    grid_check=GRID_CHECK.get()),
+                                    *feature_list).grid(row=2, column=1, sticky='w')
         
         FEATURES.pack(pady=30)
 
 
         OPTIONS = tk.Frame(INFO, bg=BACKGROUND_COLOR)
         
-        s = ttk.Style()                     # Creating style element
-        s.configure('Wild.TRadiobutton',    # First argument is the name of style. Needs to end with: .TRadiobutton
-                background=BACKGROUND_COLOR,             # Setting background to our specified color above
-                foreground='black')      
+        global CURRENT_PLOT_TYPE, GRID_CHECK, SCALE
+        GRID_CHECK = tk.IntVar()
+        SCALE      = tk.IntVar()
+        CURRENT_PLOT_TYPE = tk.StringVar()
         
+        GRID_CHECK.set(1)
+        SCALE.set(2)
+        CURRENT_PLOT_TYPE.set('Line')
 
-        tk.Label(OPTIONS, text='Plot Type :', bg=BACKGROUND_COLOR).grid(row=0)
+
+        tk.Label(OPTIONS, 
+                text='Plot Type :', 
+                bg=BACKGROUND_COLOR,
+                fg=FONT_COLOR).grid(row=0)
 
         # Options to select the PLOT TYPE
-        button1 = ttk.Radiobutton(OPTIONS, 
+        button1 = tk.Radiobutton(OPTIONS, 
                         text='Line', 
                         value='line',
-                        # style='Wild.TRadioButton',
-                        command=lambda: command.update_graph('line', plot_val_x.get(), plot_val_y.get()))
-        # button1.configure(background =BACKGROUND_COLOR)
+                        bg=BACKGROUND_COLOR,
+                        fg=FONT_COLOR,
+                        font=LARGE_FONT,
+                        variable=CURRENT_PLOT_TYPE,
+                        command=lambda: command.update_graph(CURRENT_PLOT_TYPE.get(), 
+                                                            plot_val_x.get(), 
+                                                            plot_val_y.get(),
+                                                            scale=SCALE.get(),
+                                                            grid_check=GRID_CHECK.get()))
+
+
         button1.grid(row=1, sticky='w')
         
-        button2 = ttk.Radiobutton(OPTIONS, 
+        button2 = tk.Radiobutton(OPTIONS, 
                         text='Scatter', 
                         value='scatter',
-                        # style='Wild.TRadioButton',
-                        command=lambda: command.update_graph('scatter', plot_val_x.get(), plot_val_y.get()))
-        # button2.configure(background =BACKGROUND_COLOR)
+                        bg=BACKGROUND_COLOR,
+                        fg=FONT_COLOR,
+                        font=LARGE_FONT,
+                        variable=CURRENT_PLOT_TYPE,
+                        command=lambda: command.update_graph(CURRENT_PLOT_TYPE.get(), 
+                                                            plot_val_x.get(), 
+                                                            plot_val_y.get(),
+                                                            scale=SCALE.get(),
+                                                            grid_check=GRID_CHECK.get()))
+
+
         button2.grid(row=2, sticky='w')
+
+
+
+        slider = ttk.Scale(OPTIONS, 
+                            # label='Size',
+                            from_=1,
+                            to=5,
+                            variable=SCALE,
+                            orient='horizontal',
+                            command= lambda SCALE: command.update_graph(CURRENT_PLOT_TYPE.get(), 
+                                                                       plot_val_x.get(), 
+                                                                       plot_val_y.get(), 
+                                                                       scale=float(SCALE),
+                                                                       grid_check=GRID_CHECK.get()))
+
+        slider.grid(row=3, sticky='w')
+
+        grid_check = tk.Checkbutton(OPTIONS,
+                            variable=GRID_CHECK,
+                            text='Grid',
+                            command= lambda : command.update_graph(CURRENT_PLOT_TYPE.get(), 
+                                                                             plot_val_x.get(), 
+                                                                             plot_val_y.get(),
+                                                                             scale=SCALE.get(),
+                                                                             grid_check=GRID_CHECK.get()))
+
+        grid_check.grid(row=4, sticky='w')
+
+
+
+        # dark_mode = tk.Checkbutton(OPTIONS, command=command.dark_mode).grid(row=5, sticky='w')
 
         OPTIONS.pack(pady=30)
 
@@ -194,7 +292,8 @@ class StartPage(tk.Frame):
         label = tk.Label(parent, 
                         text='It begins here!', 
                         font=LARGE_FONT,
-                        bg=BACKGROUND_COLOR).pack()
+                        bg=BACKGROUND_COLOR,
+                        fg=FONT_COLOR).pack()
 
         # the UPLOAD button
         button1 = ttk.Button(parent, text='Upload',
